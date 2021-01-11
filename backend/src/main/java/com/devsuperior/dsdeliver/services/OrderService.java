3c6289ce2570f0c44b9a5289b4094f41c160c90e
fabio brasil <fabio.fernandes@ifal.edu.br>
2021-01-10 23:44:34 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 //Poderia ser Component
 //Classe passa a ser um componente registrado que vai poder ser injetado em outros componentes
@@ -28,6 +33,8 @@ public class OrderService {
 	
 	@Autowired
 	private OrderRepository repositoty;
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@Transactional(readOnly = true) //Operação somente de leitura - Isso evita o lock no banco de dados.
 	public List<OrderDTO> findAll(){
@@ -36,4 +43,42 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x) ).collect(Collectors.toList())   ;
 	}
 
+	@Transactional 
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null,
+				dto.getAddress(),
+				dto.getLatitude(),
+				dto.getLongitude(),
+				Instant.now(),
+				OrderStatus.PENDING
+				);
+		
+		for (ProductDTO p : dto.getProducts()) {
+			//Instancia um produco com base no id do p.Não vai no banco e sim na memoria levando o que já existe
+			Product product = productRepository.getOne(p.getId());//Salva as associções dos produtos que estão neste pedido
+			
+			order.getProducts().add(product);
+		}
+		
+		order = repositoty.save(order); //guarda uma referência para o objeto salvo
+		
+		return new OrderDTO(order);
+	}
+	
+	@Transactional 
+	public OrderDTO setDelivered(Long id){//é o atualizar pedido ou order
+	//1 - vai ter que dar um getOne para pegar um objeto monitorado pelo JPA
+		Order order = repositoty.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		
+		order = repositoty.save(order);
+		
+		return new OrderDTO(order);
+		
+	
+	}
+	
+	
+	
+	
 }
